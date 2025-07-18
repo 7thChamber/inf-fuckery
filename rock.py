c8d8d2857d8855eae36a38dee631d26123074419
7thChamber <srrodrigu07@gmail.com>
2025-07-18 13:41:20 -0500

    rock.py

--- rock.py
+++ rock.py
@@ -1,8 +1,9 @@
 import random
 import time
+import sys
 
-print("Greetings,")
-
+print("Greetings")
+time.sleep(0.2)
 while True:
     choices = ['rock', 'paper', 'scissors']
     while True:
@@ -12,6 +13,15 @@ while True:
         else:
             print("Type your response")
 
+    print("Rock")
+    time.sleep(.5)
+    print("Paper")
+    time.sleep(.5)
+    print("Scissors")
+    time.sleep(.5)
+    print("SHOOT!")
+    time.sleep(2)
+
     computer_choice = random.choice(choices)
     if player_response == 'rock' and computer_choice == 'scissors' or \
     player_response == 'paper' and computer_choice == 'rock' or \
@@ -19,6 +29,7 @@ while True:
         print("You Win!")
     else:
         print("You Lose :( ")
+    time.sleep(1)
     keep_playing = input("Would you like to play again? (Y/N): ")
     if keep_playing == 'N':
         break 
